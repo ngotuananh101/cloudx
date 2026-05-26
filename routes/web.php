48c672383e3dcc7351942a12380e9b93c8b1bc93
Ngo Tuan Anh <ngotuananh2101@gmail.com>
2026-05-26 21:44:49 +0700

    fix(routes): update storage and connection routes
    - Change storage browsing route from '/s/{connection}/{path?}' to '/storage/{connection}/{path?}'
    - Update cloud connection deletion route from '/cloud-connections/{connection}' to '/connections/{connection}'
    - Update cache deletion route from '/cloud-connections/{connection}/cache' to '/connections/{connection}/cache'

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', HomeController::class)->name('dashboard');
 
     // Cloud Storage Browsing Route
-    Route::get('/s/{connection}/{path?}', [StorageBrowserController::class, 'index'])
+    Route::get('/storage/{connection}/{path?}', [StorageBrowserController::class, 'index'])
         ->name('storage.index')
         ->where('path', '.*');
     // Cloud Connections OAuth Flow
     Route::get('/oauth/{provider}/redirect', [CloudConnectionController::class, 'redirect'])->name('oauth.redirect');
     Route::get('/oauth/{provider}/callback', [CloudConnectionController::class, 'callback'])->name('oauth.callback');
-    Route::delete('/cloud-connections/{connection}', [CloudConnectionController::class, 'disconnect'])->name('cloud-connections.destroy');
-    Route::delete('/cloud-connections/{connection}/cache', [CloudConnectionCacheController::class, 'destroy'])->name('cloud-connections.cache.destroy');
+    Route::delete('/connections/{connection}', [CloudConnectionController::class, 'disconnect'])->name('cloud-connections.destroy');
+    Route::delete('/connections/{connection}/cache', [CloudConnectionCacheController::class, 'destroy'])->name('cloud-connections.cache.destroy');
 });
